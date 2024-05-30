7739c1533a2c31731080c578efbe9b401aa0d4b5
Hyunjun Lee <leehyunjune11@gmail.com>
2024-05-31 01:25:08 +0900

    day4-memo

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const mapbtn = document.getElementById("map-btn");
-
+const savememo = document.getElementById("save-memo");
 
 const darkmode = () => {
     console.log("클릭");
@@ -13,4 +13,17 @@ function googleTranslateElementInit() {
     new google.translate.TranslateElement({pageLanguage: 'ko', includedLanguages : 'en,ja,ko,zh-CN', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
   }
 
-  
+const addmemo = () =>{
+    const memoInput = document.getElementById("memo-input");
+    const memoList = document.getElementById("memo-list");
+
+    if(memoInput.value.trim() !==""){
+        const memoDiv = document.createElement('div');
+        memoDiv.className ='memo';
+        memoDiv.textContent = memoInput.value;
+        memoList.appendChild(memoDiv);
+        memoInput.value ='';
+    }
+}
+
+savememo.addEventListener("click", addmemo);
